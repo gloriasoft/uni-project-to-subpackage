9143da52da6eac60d823555da2a631b8eb87727a
junping.wu <9700616@qq.com>
2020-06-15 23:10:06 +0800

    加入了原生小程序代码的条件编译

--- projectToSubPackageConfig.js
+++ projectToSubPackageConfig.js
@@ -27,6 +27,10 @@ module.exports={
     configWxResourceKey: null,
     // 插件
     plugins: [
+        // 条件编译插件应该在混写插件之前使用
+        'jsPreProcessPlugin', // js条件编译
+        'cssPreProcessPlugin', // css条件编译
+        'htmlPreProcessPlugin', // html条件编译
         'polyfillPlugin', // 对一些js方法的polyfill
         'htmlMixinPlugin', // html混写
         'cssMixinPlugin' // css混写
